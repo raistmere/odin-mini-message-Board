cfa1c8cd6f75fe902d408ca2d40a047a32ed8bc8
raistmere <raistmere@gmail.com>
2024-04-06 11:15:26 -0500

    feat: added new message button/form functionality
    The user can now add a new message by clicking on new message button.
    This will take them to a new message form where they can fill out and
    submit the message. This will redirect them to the homepage with the
    updated messages.

--- routes/new.js
+++ routes/new.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('new', { title: 'New' });
+  res.render('form', { title: 'New' });
 });
 
 module.exports = router;
